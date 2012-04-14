bbc71bc4b8ff1aa4fbeb091fac077bdc599a8aaa
Jobin <jobinvcm@gmail.com>
2012-04-14 02:51:35 -0700

    Testing Accelerometer 1st tym

--- avrhidmouse.c
+++ avrhidmouse.c
@@ -4,22 +4,23 @@
 #include "adc.h"
 void main()
 {
+DDRC=0xff;
 InitLCD(LS_ULINE);
 LCDWriteString("Hello World");
 adc_init();
+PORTC=0x00;
 while(1)
 {
-_delay_ms(500);
-LCDClear();
-LCDWriteInt(getdata(0),4);
 
 _delay_ms(500);
-LCDClear();
+LCDWriteString("x=");
+LCDWriteInt(getdata(0),4);
+
+LCDWriteString("y=");
 LCDWriteInt(getdata(1),4);
 
 
-_delay_ms(500);
-LCDClear();
+LCDWriteString("z=");
 LCDWriteInt(getdata(2),4);
 }
 }
